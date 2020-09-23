04491b7852761be4abbd20dc9e0416b0a0bb32a7
Andre Le <dre@Andres-MBP-35.home>
2020-09-22 22:19:35 -0400

    added instagram handles to show page, and added readME

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# project2
+# TooLowForComfort Website
+
+http://too-low-for-comfort.herokuapp.com/toolow
+
+This website is for car enthusiasts that showcases their cars and posts them on a blog. The user has to create an account on the website and can only edit & delete their own posts. On the home page, it shows the TooLowForComfort shop with some of the merch. Unfortunately it is all currently sold out, but will restock in the future.
+
+I used mongodb Atlas as my database to store my posts and heroku to host my project. Also used express and mongoose to make a full CRUD app. And lastly used css to style the website.
+
+In the future I would like to add a comment section on each post for users to be able to comment on other user's posts.
--- models/seed.js
+++ models/seed.js
@@ -3,6 +3,7 @@ module.exports = [
     name: 'Andre Le',
     car: 'Acura TSX',
     description: "Andre's stanced TSX. Running BC Racing Extreme Low coilovers with swift springs and 3pc GMR wheels.",
+    instagram: '@static_dre',
     img: 'https://i.imgur.com/pBWaa2u.jpg',
     img2: 'https://i.imgur.com/Iebk0Y7.jpg',
     img3: 'https://i.imgur.com/HSycVBH.jpg'
@@ -11,6 +12,7 @@ module.exports = [
     name: 'Eric Riley',
     car: 'Hyundai Genesis',
     description: "Eric's widebody Genesis. Running Airlift 3P bags, Enkei Algernon 3pc wheels and a wide body kit.",
+    instagram: '@e_rile',
     img: 'https://i.imgur.com/1IIdRvN.jpg',
     img2: 'https://i.imgur.com/YVgZFp1.jpg',
     img3: 'https://i.imgur.com/rpOwW9o.jpg'
@@ -19,6 +21,7 @@ module.exports = [
     name: 'Jon Salavador',
     car: 'Chevrolet Camaro',
     description: "Jon's not your average Camaro. Running BC Racing coilovers and Work VS-XX 3pc wheels.",
+    instagram: '@jsal120',
     img: 'https://i.imgur.com/TiLsJLC.jpg',
     img2: 'https://i.imgur.com/X2gwdb6.jpg',
     img3: ''
